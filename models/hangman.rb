3d2a8ecde077c208d0c18ec71baf426f676c23fd
deniseb89 <denise_b89@hotmail.com>
2014-11-08 16:40:07 -0500

    handles when a guessed letter appears more than once in a word

--- models/hangman.rb
+++ models/hangman.rb
@@ -3,17 +3,23 @@ class Hangman < ActiveRecord::Base
   belongs_to :user
 
   def guess_letter(letter)
-    index = self.random_word.index(letter)
-    if index # What if there is the same letter more than once?
-      state = self.game_state.dup
-      state[index] = letter
-      self.update({game_state: state})
-    else
-      bad = self.bad_guesses
-      bad+=letter
-      self.update({bad_guesses: bad})
-    end
-    self.game_state
+  	if self.word.include?(letter)
+  		letter_locations = []
+  		self.word.chars.each_with_index do |word_letter, index|
+  			if word_letter == letter
+  				letter_locations << index
+  			end
+  		end
+  		state = self.game_state.dup
+  		letter_locations.each do |letter_index|
+  			state[letter_index] = letter
+  		end
+  		self.update({game_state: state})
+  	else
+	  	bad = self.bad_guesses
+	  	bad += letter
+	  	self.update({bad_guesses: bad})
+  	end
+  	self.game_state
   end
-
 end
